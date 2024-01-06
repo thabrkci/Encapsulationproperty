d953dd92b5b8aaaef0c7f6e0df32d15dfa2646f6
thabrkci <alpacinotaha@gmail.com>
2024-01-06 13:39:16 +0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,99 +1,112 @@
-﻿namespace Kapsülleme_ve_Property_Kavramı;
+﻿using System;
 
-class Program
+namespace Encapsulationproperty
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("******Kapsülleme Ve Property******");
-        Student students = new Student();
-        students.Name = "Taha";
-        students.Surname = "BÖREKCİ";
-        students.Studentno =259;
-        students.Classno = 1;
-        students.classplus();
-        students.Studentex();
-        Student studens1 = new Student("Taha","BÖREKCİ",1,256);
-        studens1.classeksi();
-        studens1.classeksi();
-        students.Studentex();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Encapsulation and Property******");
 
+            // Creating an instance of the Student class using the default constructor
+            Student student1 = new Student();
+            student1.Name = "Taha";
+            student1.Surname = "BÖREKCİ";
+            student1.StudentNo = 259;
+            student1.ClassNo = 1;
 
+            // Incrementing the class number using the classplus method
+            student1.ClassPlus();
 
+            // Displaying the information of the student using the Studentex method
+            student1.StudentEx();
+
+            // Creating another instance of the Student class using the parameterized constructor
+            Student student2 = new Student("Taha", "BÖREKCİ", 1, 256);
+
+            // Decrementing the student number using the classeksi method
+            student2.ClassEksi();
+            student2.ClassEksi();
+
+            // Displaying the information of the second student
+            student2.StudentEx();
+        }
     }
 
-
-
+    // Student class definition
     class Student
     {
-      private string name;
-      private string surname;
-      private int studentno;
-      private int classno;
+        // Private fields for encapsulation
+        private string name;
+        private string surname;
+        private int studentNo;
+        private int classNo;
+        private int schoolNo;
 
-
-      public string Name
-      {
-       get {return name;}
-       set {name = value;}
-      }
-
-      public string Surname {get => surname; set => surname = value;}
-      public int Studentno 
-      {get => studentno; 
-       set
-       {
-        if(value <1)
+        // Public properties with encapsulation
+        public string Name
         {
-            Console.WriteLine("Sınıf en az 1 olabilir");
-            studentno=1;
+            get { return name; }
+            set { name = value; }
         }
-        else 
-        studentno = value;
 
-       } 
-       }
-      public int Classno {get => classno; set => classno = value;}
+        public string Surname
+        {
+            get => surname;
+            set => surname = value;
+        }
 
+        public int StudentNo
+        {
+            get => studentNo;
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("Class number cannot be less than 1");
+                    studentNo = 1;
+                }
+                else
+                {
+                    studentNo = value;
+                }
+            }
+        }
 
-      public Student(string İsim, string soyisim, int ögrencino, int sınıf)
-      {
-        İsim = Name;
-        soyisim = Surname;
-        ögrencino = Studentno;
-        sınıf = Classno;
-      }
+        public int ClassNo { get => classNo; set => classNo = value; }
 
-      public Student(){}
+        // Parameterized constructor for initializing object with provided values
+        public Student(string name, string surname, int studentNo, int classNo)
+        {
+            Name = name;
+            Surname = surname;
+            StudentNo = studentNo;
+            ClassNo = classNo;
+        }
 
+        // Default constructor needed for the Studentex method not to produce errors
+        public Student() { }
 
-      public void Studentex()
-      {
-       Console.WriteLine("*****Student Ex******");
-       Console.WriteLine("Student Name        :{0}", this.Name);
-       Console.WriteLine("Student Surname        :{0}", this.Surname);
-       Console.WriteLine("Student Studentno        :{0}", this.Studentno);
-       Console.WriteLine("Student Classno        :{0}", this.Classno);
-      }
-      
+        // Method to display student information
+        public void StudentEx()
+        {
+            Console.WriteLine("*****Student Information******");
+            Console.WriteLine("Student Name        : {0}", this.Name);
+            Console.WriteLine("Student Surname     : {0}", this.Surname);
+            Console.WriteLine("Student StudentNo   : {0}", this.StudentNo);
+            Console.WriteLine("Student ClassNo     : {0}", this.ClassNo);
+        }
 
-      public void classplus()
-      {
-       this.Classno = this.Classno +1;
+        // Method to increment the class number
+        public void ClassPlus()
+        {
+            this.ClassNo = this.ClassNo + 1;
+        }
 
-      }
-
-      public void classeksi()
-      {
-       this.Studentno = this.Studentno -1;
-
-      }
-
-
-
+        // Method to decrement the student number
+        public void ClassEksi()
+        {
+            this.StudentNo = this.StudentNo - 1;
+        }
     }
-
-  
-
-   
-    
 }
---
+++ Readme.md
@@ -0,0 +1,12 @@
+ Herkese Selam,
+ Student sınıfı, kapsülleme için özel alanlara ve bunlara erişmek ve değiştirmek için genel özelliklere sahip bir öğrenciyi temsil eder. 
+ Sınıf, nesneyi sağlanan değerlerle başlatmak için parametrelendirilmiş bir kurucu ve başlangıç değerleri sağlamadan örnekler oluşturmak için varsayılan bir kurucu içerir.
+ Program, öğrenci bilgilerini işlemek ve görüntülemek için bu kurucuların, özelliklerin ve yöntemlerin kullanımını göstermektedir. 
+ ------------------------------------------------------------------
+ Hello everyone,
+ The Student class represents a student with private fields for encapsulation and public properties to access and modify them. 
+ The class contains a parameterized constructor to initialize the object with provided values and a default constructor to create instances without providing initial values.
+ The program demonstrates the use of these constructors, properties and methods to manipulate and display student information. 
+You can find the necessary details in the comments.
+Thank you
+ ------------------------------------------------------------------
